23b79f1e3ef369bc91fcfd56598b857af4817fbd
bruno moura <brunnofarias16@gmail.com>
2024-01-29 17:14:56 -0300

    compnente sobre mim criado

---
+++ app/Components/SobreMim/page.tsx
@@ -0,0 +1,26 @@
+function SobreMim() {
+  return (
+    <>
+          <div className="m-6 p-4">
+              <h2 className="font-bold text-[25px] text-center">
+                  Sobre mim
+              </h2>
+        <h3 className="text-gray-100">
+          Sou um profissional versátil com expertise em tecnologias como
+          JavaScript, HTML5, CSS3, SQL e Python, dominando do básico ao avançado
+          no desenvolvimento web, incluindo o uso de frameworks como React.
+          Minha bagagem inclui uma visão ágil para os negócios, com cursos como
+          "A Empresa Ágil" e "Gestão Ágil", e uma mentalidade empreendedora
+          embasada em Lean Startup e Business Model Canvas. Minha dedicação ao
+          autodesenvolvimento, com foco em produtividade e aprendizado contínuo,
+          complementa minhas habilidades técnicas, proporcionando uma visão
+          holística dos processos e das necessidades organizacionais. Combinando
+          habilidades sólidas, pensamento estratégico e adaptabilidade, estou
+          pronto para agregar valor em projetos desafiadores, trazendo inovação
+          e eficiência para cada iniciativa.
+        </h3>
+      </div>
+    </>
+  );
+}
+export default SobreMim;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import NavBar from "./Components/NavBar/page";
 import Perfil from "./Components/Perfil/page";
+import SobreMim from "./Components/SobreMim/page";
 
 export default function Home() {
     return (
         <main className="bg-gray-900 w-screen h-screen">
             <NavBar />
             <Perfil />
+            <SobreMim />
         </main>
   );
 }
