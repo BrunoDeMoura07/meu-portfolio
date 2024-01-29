0b8d544bd45563e3b85a004ccf6dd6699c6e74db
bruno moura <brunnofarias16@gmail.com>
2024-01-29 17:57:25 -0300

    componente sobre mim estilizado

--- app/Components/Perfil/page.tsx
+++ app/Components/Perfil/page.tsx
@@ -1,12 +1,14 @@
 function Perfil() {
   return (
     <>
-      <div className="flex m-6 bg-gray-800 rounded-lg border-black p-4">
+      <div className="flex m-6 border-black p-4">
         <div className="text-center">
           <h1 className="font-bold text-[50px] bg-clip-text bg-gradient-to-r to-cyan-500 from-purple-500 text-transparent">
             React developer
           </h1>
-          <span className="text-[30px] text-orange-500 font-semibold">Bruno Moura</span>
+          <span className="text-[30px] text-orange-500 font-semibold">
+            Bruno Moura
+          </span>
         </div>
       </div>
     </>
--- app/Components/SobreMim/page.tsx
+++ app/Components/SobreMim/page.tsx
@@ -1,23 +1,32 @@
 function SobreMim() {
   return (
     <>
-          <div className="m-6 p-4">
-              <h2 className="font-bold text-[25px] text-center">
-                  Sobre mim
-              </h2>
+      <div className="m-6 p-4 bd- bg-gray-800 rounded-xl">
+        <h2 className="font-bold text-[18px] text-center text-purple-500">
+          Sobre mim
+        </h2>
         <h3 className="text-gray-100">
           Sou um profissional versátil com expertise em tecnologias como
-          JavaScript, HTML5, CSS3, SQL e Python, dominando do básico ao avançado
-          no desenvolvimento web, incluindo o uso de frameworks como React.
-          Minha bagagem inclui uma visão ágil para os negócios, com cursos como
-          "A Empresa Ágil" e "Gestão Ágil", e uma mentalidade empreendedora
-          embasada em Lean Startup e Business Model Canvas. Minha dedicação ao
+          <span className="text-cyan-400 font-semibold">
+            {" "}
+            JavaScript, HTML5, CSS3, SQL e Python{" "}
+          </span>
+          , dominando do básico ao avançado no desenvolvimento web, incluindo o
+          uso de frameworks como{" "}
+          <span className="text-cyan-500 font-semibold">React</span>. Minha
+          bagagem inclui uma visão ágil para os negócios, com cursos como <span className="font-semibold text-orange-500">"A
+          Empresa Ágil" e "Gestão Ágil"</span>, e uma mentalidade empreendedora
+          embasada em <span className="font-semibold text-orange-500">Lean Startup e Business Model Canvas</span>. Minha dedicação ao
           autodesenvolvimento, com foco em produtividade e aprendizado contínuo,
           complementa minhas habilidades técnicas, proporcionando uma visão
-          holística dos processos e das necessidades organizacionais. Combinando
-          habilidades sólidas, pensamento estratégico e adaptabilidade, estou
-          pronto para agregar valor em projetos desafiadores, trazendo inovação
-          e eficiência para cada iniciativa.
+          holística dos processos e das necessidades organizacionais.
+          <span className="text-violet-500 font-semibold">
+            {" "}
+            Combinando habilidades sólidas, pensamento estratégico e
+            adaptabilidade,
+          </span>{" "}
+          estou pronto para agregar valor em projetos desafiadores, trazendo
+          inovação e eficiência para cada iniciativa.
         </h3>
       </div>
     </>
